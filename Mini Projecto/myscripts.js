8e11ddd30d7ec116cfbe727ef6e22ca403baeff2
NunoV28 <99043453+NunoV28@users.noreply.github.com>
2022-03-21 19:58:59 +0000

    Add files via upload

---
+++ Mini Projecto/myscripts.js
@@ -0,0 +1,14 @@
+function TransformUI() {
+document.getElementById("texto").innerHTML = "Ultra Instinct ";
+document.getElementById("img").src="newSource.png";
+}
+
+function Transform() {
+document.getElementById("texto").innerHTML = "Normal Form ";
+document.getElementById("img").src="ngoku.jpeg";
+}
+
+function kame() {
+document.getElementById("texto").innerHTML = "UI Kamehameha ";
+document.getElementById("img").src="https://giffiles.alphacoders.com/208/208598.gif";
+}
